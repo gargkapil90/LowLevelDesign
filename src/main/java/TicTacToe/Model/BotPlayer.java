a0af42a28a0902238f42a17bfda56549e3d14ad8
Kapil Garg <gargkapil90@gmail.com>
2024-03-01 23:14:48 +0530

    adding getters and setters

--- src/main/java/TicTacToe/Model/BotPlayer.java
+++ src/main/java/TicTacToe/Model/BotPlayer.java
@@ -2,4 +2,12 @@ package TicTacToe.Model;
 
 public class BotPlayer extends Player{
     DifficultyLevel difficultyLevel;
+
+    public DifficultyLevel getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    public void setDifficultyLevel(DifficultyLevel difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
 }
